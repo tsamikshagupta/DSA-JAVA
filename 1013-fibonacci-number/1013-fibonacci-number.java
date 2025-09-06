0e3b7862fbbe85e40a760fa4b345989df1f8eb38
samiksha Gupta <samikshaterala9@gmail.com>
2025-09-06 15:35:44 +0530

    Time: 0 ms (100%), Space: 40.6 MB (27.99%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static int fib(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        int prev =0, curr =1;
+        for(int i=2;i<=n;i++){
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+        
+    }
+    
+}
