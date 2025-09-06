9fd4d345e1fd5c433a548101be4ff6c3cf492772
samiksha Gupta <samikshaterala9@gmail.com>
2025-09-06 15:33:53 +0530

    Time: 0 ms (100%), Space: 49.5 MB (28.24%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public static void reverseString(char[] s) {
+        int left =0;
+        int right = s.length-1;
+        while(left<right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        
+    }
+
+}
