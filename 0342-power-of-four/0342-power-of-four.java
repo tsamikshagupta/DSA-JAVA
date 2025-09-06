a90c796ceb8fe9e5f53157a9a1104ea441f8f605
samiksha Gupta <samikshaterala9@gmail.com>
2025-09-06 15:33:33 +0530

    Time: 0 ms (100%), Space: 41.1 MB (35.64%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public static boolean isPowerOfFour(int n) {
+        if(n<=0) return false;
+        while(n>1){
+            if(n%4!=0){
+                return false;
+            }
+            n = n/4;
+        }
+        return n==1;
+    }
+   
+}
