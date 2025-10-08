610485527c0f429f5e8f45fe72690874295af120
samiksha Gupta <samikshaterala9@gmail.com>
2025-10-09 00:57:23 +0530

    Time: 16 ms (13.46%), Space: 47.9 MB (5.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,12 @@
 import java.util.*;
-
 class Solution {
     public boolean isPalindrome(String str) {
         String s = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
         return checkPalindrome(s, 0, s.length() - 1);
     }
-
     private boolean checkPalindrome(String str, int left, int right) {
         if (left >= right) return true;
         if (str.charAt(left) != str.charAt(right)) return false;
         return checkPalindrome(str, left + 1, right - 1);
     }
-
-   
 }
