015b33fd326e23025e5bc97077dd5929aae8ac2c
samiksha Gupta <samikshaterala9@gmail.com>
2025-10-19 01:19:36 +0530

    Time: 1 ms (62.87%), Space: 42.2 MB (78.53%) - LeetHub

---
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.offerFirst(root);   
+        boolean leftToRight = true;  
+        while (!deque.isEmpty()) {
+            int size = deque.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = leftToRight ? deque.pollFirst() : deque.pollLast();
+                level.add(node.val);
+                if (leftToRight) {
+                    if (node.left != null) deque.offerLast(node.left);
+                    if (node.right != null) deque.offerLast(node.right);
+                } else {
+                    if (node.right != null) deque.offerFirst(node.right);
+                    if (node.left != null) deque.offerFirst(node.left);
+                }
+            }
+            result.add(level);
+            leftToRight = !leftToRight;  
+        }
+        return result;
+    }
+}
