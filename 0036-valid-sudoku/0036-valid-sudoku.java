48687ab9d1f9d1017dc2673e28f3493c66f4caea
samiksha Gupta <samikshaterala9@gmail.com>
2025-10-20 00:07:06 +0530

    Time: 9 ms (20.03%), Space: 45.1 MB (13.6%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> rows = new HashSet<>();
+        Set<String> cols = new HashSet<>();
+        Set<String> boxes = new HashSet<>();
+
+        for(int i=0;i<9;i++){
+            for(int j = 0 ; j<9;j++){
+                char cell = board[i][j];
+                if(cell == '.') continue;
+                String rowKey = i + "r" + cell;
+                String colKey = j + "c" + cell;
+                String boxKey = (i/3) * 3 + (j/3) + "b" + cell;
+                if(rows.contains(rowKey) || cols.contains(colKey) || boxes.contains(boxKey)) return false;
+                rows.add(rowKey);
+                cols.add(colKey);
+                boxes.add(boxKey);
+
+            }
+        }
+        return true;
+        
+    }
+}
