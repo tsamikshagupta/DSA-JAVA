bca9540ef1f28fbf4160f7b438549d82415d5b7a
samiksha Gupta <samikshaterala9@gmail.com>
2025-11-12 01:38:07 +0530

    Time: 1 ms (80.68%), Space: 43 MB (18.91%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0, j = 0;
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i++));
+            sb.append(word2.charAt(j++));
+        }
+        if (i < word1.length()) sb.append(word1.substring(i));
+        if (j < word2.length()) sb.append(word2.substring(j));
+        return sb.toString();
+        
+    }
+}
