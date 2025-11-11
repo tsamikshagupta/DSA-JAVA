d07ca477f4d55528ed07fa55427d18758e8aedab
samiksha Gupta <samikshaterala9@gmail.com>
2025-11-11 23:07:57 +0530

    Time: 1 ms (65.57%), Space: 43.8 MB (6.8%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -16,26 +16,26 @@
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
-        if (root == null) return result;
+        if(root == null)return result;
         Deque<TreeNode> deque = new LinkedList<>();
-        deque.offerFirst(root);   
-        boolean leftToRight = true;  
-        while (!deque.isEmpty()) {
+        deque.offerFirst(root);
+        boolean leftToRight = true;
+        while(!deque.isEmpty()){
             int size = deque.size();
             List<Integer> level = new ArrayList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = leftToRight ? deque.pollFirst() : deque.pollLast();
+            for(int i = 0 ; i < size ; i++){
+                TreeNode node = leftToRight? deque.pollFirst() : deque.pollLast();
                 level.add(node.val);
-                if (leftToRight) {
-                    if (node.left != null) deque.offerLast(node.left);
-                    if (node.right != null) deque.offerLast(node.right);
-                } else {
-                    if (node.right != null) deque.offerFirst(node.right);
-                    if (node.left != null) deque.offerFirst(node.left);
+                if(leftToRight){
+                    if(node.left!=null) deque.offerLast(node.left);
+                    if(node.right!=null) deque.offerLast(node.right);
+                }else{
+                    if(node.right!=null)deque.offerFirst(node.right);
+                    if(node.left!=null)deque.offerFirst(node.left);
                 }
             }
             result.add(level);
-            leftToRight = !leftToRight;  
+            leftToRight = !leftToRight;
         }
         return result;
     }
