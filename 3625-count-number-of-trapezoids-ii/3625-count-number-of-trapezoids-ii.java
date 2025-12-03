f94aa0255ef58955b361030de285ae18512714ee
samiksha Gupta <samikshaterala9@gmail.com>
2025-12-03 19:00:41 +0530

    Time: 733 ms (85.16%), Space: 278.2 MB (70.31%) - LeetHub

--- 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.java
+++ 3625-count-number-of-trapezoids-ii/3625-count-number-of-trapezoids-ii.java
@@ -1,17 +1,13 @@
-import java.util.*;
-
 public class Solution {
 
-    static final int BIAS = 1 << 11;  // 2048
-
-    // Pack 2 integers into one 32-bit key
+    static final int BIAS = 1 << 11;  
     static int pack2(int a, int b) {
         long A = (long)(a + BIAS);
         long B = (long)(b + BIAS);
         return (int)((A << 16) | B);
     }
 
-    // Pack 3 integers into one 64-bit key
+
     static long pack3(int a, int b, int c) {
         long A = (long)(a + BIAS);
         long B = (long)(b + BIAS);
@@ -19,7 +15,6 @@ public class Solution {
         return (A << 50) | (B << 30) | C;
     }
 
-    // Pack 4 integers into one 64-bit key
     static long pack4(int a, int b, int c, int d) {
         long A = (long)(a + BIAS);
         long B = (long)(b + BIAS);
@@ -43,8 +38,6 @@ public class Solution {
 
         int n = points.length;
         int nn = n * (n - 1);
-
-        // unordered_map<long long,int> equivalents
         HashMap<Long, Integer> coeff = new HashMap<>(nn);
         HashMap<Long, Integer> midPointWslope = new HashMap<>(nn);
         HashMap<Integer, Integer> slope = new HashMap<>(nn);
@@ -59,13 +52,9 @@ public class Solution {
             for (int j = i + 1; j < n; j++) {
                 int x1 = points[j][0];
                 int y1 = points[j][1];
-
-                // line : ax + by + c = 0
                 int a = y1 - y0;
                 int b = x0 - x1;
                 int c = y0 * x1 - y1 * x0;
-
-                // fix sign so first non-zero coefficient is positive
                 if (a == 0 && b < 0) {
                     b = -b;
                     c = -c;
@@ -84,7 +73,6 @@ public class Solution {
                 long mid = pack2(x0 + x1, y0 + y1);
                 long midSlope = pack4(x0 + x1, y0 + y1, a / gm, b / gm);
 
-                // cnt += slope++  - coeff++ - midpoint++ + midSlope++
                 cnt += slope.getOrDefault(slopeKey, 0);
                 slope.put(slopeKey, slope.getOrDefault(slopeKey, 0) + 1);
 
