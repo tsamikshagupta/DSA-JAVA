ff585bfe79daab12c345666e995f2fd8ce3938a4
samiksha Gupta <samikshaterala9@gmail.com>
2026-01-21 23:27:18 +0530

    Time: 0 ms (100%), Space: 47.6 MB (32.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,7 +2,6 @@ class Solution {
 
     private void dfs(int node, int[][] isConnected, boolean[] visited) {
         visited[node] = true;
-
         for (int j = 0; j < isConnected.length; j++) {
             if (isConnected[node][j] == 1 && !visited[j]) {
                 dfs(j, isConnected, visited);
@@ -14,7 +13,6 @@ class Solution {
         int n = isConnected.length;
         boolean[] visited = new boolean[n];
         int provinces = 0;
-
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 dfs(i, isConnected, visited);
