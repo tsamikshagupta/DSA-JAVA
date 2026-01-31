4c43c04fd5943039ba3a3baa252d2147325b0fc5
samiksha Gupta <samikshaterala9@gmail.com>
2026-01-31 20:05:44 +0530

    Time: 48 ms (9.18%), Space: 42.2 MB (73.17%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public static int findTheWinner(int n, int k) {
+        int winner = 0;
+        for(int i = 1 ; i <= n ; i++){
+            winner = (winner + k) % i;
+        }
+        return winner + 1;
+    }
+}
