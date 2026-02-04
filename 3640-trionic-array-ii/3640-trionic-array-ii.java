b2b9ce348e7263789fb49654e5837836e4c1228c
samiksha Gupta <samikshaterala9@gmail.com>
2026-02-04 20:30:13 +0530

    Time: 5 ms (53.85%), Space: 94.9 MB (76.92%) - LeetHub

--- 3640-trionic-array-ii/3640-trionic-array-ii.java
+++ 3640-trionic-array-ii/3640-trionic-array-ii.java
@@ -1,19 +1,14 @@
 class Solution {
     public long maxSumTrionic(int[] nums) {
         long NEG = Long.MIN_VALUE / 4;
-
         long inc1 = nums[0];
         long inc2 = NEG;
         long dec = NEG;
         long tri2 = NEG;
-
         long ans = NEG;
-
         for (int i = 1; i < nums.length; i++) {
             long x = nums[i];
-
             long nInc1, nInc2, nDec, nTri2;
-
             if (nums[i - 1] < nums[i]) {
                 nInc1 = Math.max(x, inc1 + x);
                 nInc2 = Math.max(inc2 == NEG ? NEG : inc2 + x, inc1 + x);
