fb6f992e5b77e321f5aa6b7acba89cf6698048d7
samiksha Gupta <samikshaterala9@gmail.com>
2026-02-15 01:42:38 +0530

    Time: 4 ms (32.01%), Space: 48.7 MB (9.71%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -6,7 +6,7 @@ class Solution {
             for (int c = 0; c <= r; c++) {
                 double overflow = Math.max(0.0, dp[r][c] - 1.0);
                 if (overflow > 0) {
-                    dp[r + 1][c]     += overflow / 2.0;
+                    dp[r + 1][c]+= overflow / 2.0;
                     dp[r + 1][c + 1] += overflow / 2.0;
                 }
             }
