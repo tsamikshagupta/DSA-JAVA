7b85ca687c9d192f3d0fff8143424b71d72a0430
samiksha Gupta <samikshaterala9@gmail.com>
2026-03-08 00:43:28 +0530

    Time: 23 ms (70.34%), Space: 47.3 MB (66.95%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,27 +1,38 @@
 class Solution {
     public int minFlips(String s) {
+
         int n = s.length();
-        String ds = s + s;
-        int d1 = 0 , d2 = 0;
+        String ss = s + s;
+
+        int diff1 = 0;
+        int diff2 = 0;
         int ans = Integer.MAX_VALUE;
-        for(int i = 0 ; i < ds.length(); i++){
-            char c = ds.charAt(i);
-            char a = (i % 2 == 0) ? '0' : '1';
-            char b = (i % 2 == 0) ? '1' : '0';
-            if(c != a) d1++;
-            if(c != b) d2++;
+
+        for(int i = 0; i < ss.length(); i++){
+
+            char c = ss.charAt(i);
+
+            char p1 = (i % 2 == 0) ? '0' : '1';
+            char p2 = (i % 2 == 0) ? '1' : '0';
+
+            if(c != p1) diff1++;
+            if(c != p2) diff2++;
+
             if(i >= n){
-                char p = ds.charAt(i - n);
-                char pa = ((i - n) % 2 == 0) ? '0' : '1';
-                char pb = ((i - n) % 2 == 0) ? '1' : '0';
-                if (p != pa) d1--;
-                if (p != pb) d2--;
+                char prev = ss.charAt(i-n);
+
+                char oldp1 = ((i-n) % 2 == 0) ? '0' : '1';
+                char oldp2 = ((i-n) % 2 == 0) ? '1' : '0';
+
+                if(prev != oldp1) diff1--;
+                if(prev != oldp2) diff2--;
             }
-            if(i >= n - 1){
-                ans = Math.min(ans , Math.min(d1, d2));
+
+            if(i >= n-1){
+                ans = Math.min(ans, Math.min(diff1, diff2));
             }
         }
+
         return ans;
-        
     }
 }
