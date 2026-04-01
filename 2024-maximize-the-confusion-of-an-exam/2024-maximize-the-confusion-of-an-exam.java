fc615e05b0ce71886303bf412e5fbdec10cc08cb
samiksha Gupta <samikshaterala9@gmail.com>
2026-04-01 22:50:12 +0530

    Time: 42 ms (7.59%), Space: 47 MB (6.16%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -9,9 +9,9 @@ class Solution {
             Character ch = s.charAt(i);
             map.put(ch, map.getOrDefault(ch , 0) + 1);
             int curr= map.get(ch);
-            max = Math.max(max,curr);
+            max = Math.max(max , curr);
             int win = (i - l + 1);
-            int m = win-max;
+            int m = win - max;
             while(m > k){
                 Character left = s.charAt(l);
                 map.put(left, map.get(left) - 1);
