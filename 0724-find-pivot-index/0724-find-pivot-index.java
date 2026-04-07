41106ab90894b9755a92888c3a027e8463009228
Samiksha Terala <samikshaterala9@gmail.com>
2026-04-08 00:40:40 +0530

    Time: 1 ms (98.66%), Space: 47.5 MB (29.86%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int value : nums) {
+            total += value;
+        }
+        int left = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int current = nums[i];
+            int right = total - left - current;
+            if (left == right) {
+                return i;
+            }
+            left += current;
+        }
+
+        return -1;
+    }
+}
