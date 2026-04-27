ad6b0cd4763a2ef54776736e19afc56a1c39a80c
Samiksha Terala <samikshaterala9@gmail.com>
2026-04-27 16:53:31 +0530

    Time: 17 ms (80.09%), Space: 88.2 MB (87.31%) - LeetHub

---
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean hasValidPath(int[][] g) {
+        int m = g.length, n = g[0].length;
+        int[][][] d = {
+            {},
+            {{0,-1},{0,1}},
+            {{-1,0},{1,0}},
+            {{0,-1},{1,0}},
+            {{0,1},{1,0}},
+            {{0,-1},{-1,0}},
+            {{0,1},{-1,0}}
+        };
+        boolean[][] vis = new boolean[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0});
+        vis[0][0] = true;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int r = cur[0], c = cur[1];
+            if (r == m-1 && c == n-1) return true;
+            for (int[] mv : d[g[r][c]]) {
+                int nr = r + mv[0];
+                int nc = c + mv[1];
+                if (nr < 0 || nc < 0 || nr >= m || nc >= n || vis[nr][nc]) continue;
+                for (int[] back : d[g[nr][nc]]) {
+                    if (nr + back[0] == r && nc + back[1] == c) {
+                        vis[nr][nc] = true;
+                        q.offer(new int[]{nr, nc});
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+}
