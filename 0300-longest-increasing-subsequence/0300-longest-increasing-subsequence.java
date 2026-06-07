1f6938db51400bbb314667ed18fa447eace96dbf
Samiksha Terala <samikshaterala9@gmail.com>
2026-06-08 01:36:06 +0530

    Time: 6 ms (86.14%), Space: 46.3 MB (31.95%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> tails = new ArrayList<>();
+        for (int num : nums) {
+            int left = 0, right = tails.size();
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (tails.get(mid) < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            if (left == tails.size()) {
+                tails.add(num);
+            } else {
+                tails.set(left, num);
+            }
+        }
+
+        return tails.size();
+    }
+}
