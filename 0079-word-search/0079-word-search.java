4aef09da5029af361f1ded169beeab9e3f70a68d
Samiksha Terala <samikshaterala9@gmail.com>
2026-07-11 10:53:56 +0530

    Time: 129 ms (81.11%), Space: 42.8 MB (87.69%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
         int m = board.length;
-        int n = board[0].length;
-        
+        int n = board[0].length; 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (board[i][j] == word.charAt(0)) {
